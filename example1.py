f783679d96847c0dcd3f510d935dd8cc2e0f0ff1
duaneloh <duaneloh@gmail.com>
2011-12-07 14:24:15 +0100

    Made a more comprehensive example.

--- example1.py
+++ example1.py
@@ -2,9 +2,25 @@
 
 import makeMonsters
 
-# instantiate monster generator
+# instantiate monster generator with default parameters
 m = makeMonsters.monsterGenerator()
-# create the particle
+
+# create the particle and particle support (for phasing)
 m.makeParticle()
-# write the particle to the default filename (density.dat)
+# write the particle and support to the default filename (density.dat and support.dat)
 m.writeDensityToFile()
+m.writeSupportToFile()
+
+# define canonical beamstop and detector, then create 3D Fourier intensities
+m.diffract()
+# write detector pixel positions (detector.dat)
+m.writeDetectorToFile() 
+# write 3D intensities (intensity.dat)
+m.writeIntensitiesToFile()
+
+# Could use m.writeAllOuputToFile() aggregate function to perform all the write functions above
+
+# Uncomment these lines to view created monsters
+# m.showDensity()
+# m.showLogIntensity()
+# m.showLogIntensitySlices()
